b0d6f333e8179f78c1feac819282f828113ba454
Noel-elon <noelnwaelugo@gmail.com>
2020-02-02 20:19:58 +0100

    Dao and Entity

---
+++ app/src/main/java/com/example/mynote/NoteDatabase.java
@@ -0,0 +1,29 @@
+package com.example.mynote;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.io.NotActiveException;
+
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+
+}
